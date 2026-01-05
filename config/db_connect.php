541d31e013f346d75b780b037c9bc994f5ae03e9
Marliva <cedriclevasseur76@gmail.com>
2026-01-05 13:48:05 +0100

    modif connexion

--- config/db_connect.php
+++ config/db_connect.php
@@ -11,8 +11,8 @@ try {
         $user,
         $pass,
         [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            PDO::ATTR_ERRMODE               =>      PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE    =>      PDO::FETCH_ASSOC
         ]
     );
 } catch (PDOException $e) {
